502d6de9fb45fb8fb32e1d47ce4929cfcd017e93
raoyuuuu <raoyuuuu@126.com>
2019-04-10 08:43:23 +0800

    4.10

--- src/main/java/com/fjut/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/fjut/service/impl/CustomerServiceImpl.java
@@ -18,6 +18,7 @@ public class CustomerServiceImpl implements CustomerService {
 
     private CustomerDao customerDao;
     public void setCustomerDao(CustomerDao customerDao) {
+
         this.customerDao = customerDao;
     }
 }
--- src/main/java/com/fjut/web/action/CustomerAction.java
+++ src/main/java/com/fjut/web/action/CustomerAction.java
@@ -23,6 +23,7 @@ public class CustomerAction extends ActionSupport implements ModelDriven<Custome
 
     private CustomerService customerService;
     public void setCustomerService(CustomerService customerService) {
+
         this.customerService = customerService;
     }
 
